69fb031c29aca95cd80c56674a6ec264d3b56277
contihan <daniel.hanak@outlook.cz>
2022-08-01 20:03:03 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿for (int i = 0; i < 26; i++)
+{
+	Console.Write((char)('a' + i) + "\t");
+}
+
+Console.WriteLine();
+Console.WriteLine();
+
+
+for (int i = 0; i < 26; i++)
+{
+	Console.Write((char)('A' + i) + "\t");
+}
+
+Console.WriteLine();
+Console.WriteLine();
+
+for (int i = 0; i < 10; i++)
+{
+	Console.Write((char)('0' + i) + "\t");
+}
+
+Console.WriteLine();
+
+Console.ReadKey();
